9ce702b3cc514353460bb24c28eb8061f162fad1
Chewy <95539361+chewyxrd@users.noreply.github.com>
2022-09-17 21:03:17 +0300

    Add files via upload

---
+++ source/Configurations/Server_Settings.js
@@ -0,0 +1,57 @@
+module.exports = {
+    guildID: "SUNUCU ID",
+
+    Roles: { 
+        Staff: {
+            banHammer: "YASAKLAMA YETKİLİSİ ROL ID",
+            jailHammer: "KARANTİNA YETKİLİSİ ROL ID",
+            muteHammer: "SUSTURMA YETKİLİSİ ROL ID",
+            warnHammer: "UYARI YETKİLİSİ ROL ID",
+            lowerStaff: "EN ALT YETKİLİ ROL ID",
+
+            botCommand: "EN ÜST YETKİLİ ROL ID"
+        },
+        Member: {
+            boosterRole: "BOOSTER ROL ID",
+            vipRole: "ÖZEL ÜYE ROL ID",
+
+            jailRole: "CEZALI ROL ID",
+            MutedRole: "SUSTURULMUŞ ROL ID",
+
+            Designer: "TASARIMCI ROL ID",
+            Musician: "MÜZİSYEN ROL ID",
+            Sponsor: "SPONSOR ROL ID",
+
+            Member: "ÜYE ROL ID ( KAYITSIZ OLABİLİR )",
+        }
+    },
+    Channels: {
+        Log: {
+            muteLog: "SUSTURMA LOG KANAL ID",
+            banLog: "YASAKLAMA LOG KANAL ID",
+            jailLog: "KARANTİNA LOG KANAL ID",
+            warnLog: "CEZALI LOG KANAL ID",
+            rolLog: "ROL LOG KANAL ID"
+        },
+    Meeting: {
+        Channel: "TOPLANTI KANAL ID"
+    },
+    },
+    Tag: {
+        Tag: {
+            GuildTag: "SUNUCU TAGINIZ"
+        }
+    },
+    Others: {
+        Emojis: {
+            check: "ONAY EMOJİ ID",
+            reject: "ÇARPI EMOJİ ID"
+        },
+        Points: {
+            Mute: "20",
+            Jail: "30",
+            Ban: "50",
+            Warn: "10"
+        }
+    }
+}
