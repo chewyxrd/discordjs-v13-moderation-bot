1eb26cae8079f7f8b2b16a82072318613ccbde18
Chewy <95539361+chewyxrd@users.noreply.github.com>
2022-09-17 21:03:04 +0300

    Create Client_Settings.js

---
+++ source/Configurations/Client_Settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botToken: "BOT TOKENİ",
+    clientOwner: ["SAHİP ID"],
+    prefix: ["."],
+
+    voiceChannel: "SES KANAL ID",
+
+    clientPresences: ["Chewy 💙"],
+    clientActivity: "Chewy 💙",
+    activityType: "STREAMING"
+ }
